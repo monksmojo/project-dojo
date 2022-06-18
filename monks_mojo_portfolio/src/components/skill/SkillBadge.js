f71a9a2872a62015d697c1f9bd0cfbf8d8d15995
UDIT MAKKAR <makkarudit8@gmail.com>
2022-06-19 03:31:32 +0530

    portfolio released hence removing it from here

--- monks_mojo_portfolio/src/components/skill/SkillBadge.js
+++
@@ -1,9 +0,0 @@
-function SkillBadge(props) {
-  return (
-    <div className="flex flex-col items-center w-1/2 p-6  md:w-1/3 xl:w-1/4">
-      <img src={props.badge} alt={props.badgeTxt} />
-      <h1 className="font-bold">{props.badgeTxt}</h1>
-    </div>
-  );
-}
-export default SkillBadge;
